f5acea77bbdfd75cd7742d4c43e21c9de3cd1bf8
Oriol Poveda <71832303+Oriol25@users.noreply.github.com>
2025-02-11 12:51:15 +0100

    Update Header.vue

--- components/partials/Header.vue
+++ components/partials/Header.vue
@@ -63,7 +63,7 @@ const menu = ref([
       class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
       aria-label="Sidebar"
     >
-      <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-700">
         <ul class="space-y-2 font-medium">
           <template v-for="item in menu">
             <MenuGroup
