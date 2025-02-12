46f49ff11ef21d1003e9d97db52bf7f80b2474f3
Oriol Poveda <oriolpoveda25@gmail.com>
2025-02-12 18:30:15 +0100

    featrue: add layout auth and index login

---
+++ layouts/auth.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dark:bg-gray-800">
+    <div class="h-screen p-4 sm:ml-6">
+      <div
+        class="h-full rounded-lg "
+      >
+        <div
+          class="h-full w-full p-8 mb-4"
+        >
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Header } from "#components";
+
+const { $pb } = useNuxtApp();
+
+</script>
