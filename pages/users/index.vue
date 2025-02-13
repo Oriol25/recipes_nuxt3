6c385321b7875c83314df1e8e052ee10a22fb90b
Oriol Poveda <oriolpoveda25@gmail.com>
2025-02-13 14:59:43 +0100

    users list view

---
+++ pages/users/index.vue
@@ -0,0 +1,45 @@
+<script setup>
+const headers = [
+  {
+    "type": "avatar",
+    "label": "Avatar",
+    "key": "avatar"
+  },
+  {
+    "type": "text",
+    "label": "Name",
+    "key": "name",
+    "primary": true
+  },
+  {
+    "type": "text",
+    "label": "Email",
+    "key": "email"
+  },
+  {
+    "type": "boolean",
+    "label": "Verified",
+    "key": "verified"
+  },
+  {
+    "type": "date",
+    "label": "Created",
+    "key": "created"
+  },
+  {
+    "type": "date",
+    "label": "Updated",
+    "key": "updated"
+  },
+];
+
+const { $pb } = useNuxtApp()
+const results = await $pb.collection('users').getList(1, 50)
+
+</script>
+
+<template>
+  <div>
+    <Table :headers="headers" :data="results" />
+  </div>
+</template>
